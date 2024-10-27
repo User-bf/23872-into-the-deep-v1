e6fbe315cd4bc323189b3b48dda7c7da50941235
Dave Whitmer <you@example.com>
2024-10-27 09:06:11 -0400

    Lead point following is working with visulaiztion.   Need to make algorithms for controlling the lead point now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PIDAutoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PIDAutoTesting.java
@@ -20,15 +20,15 @@ public class PIDAutoTesting extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         Pose2D initialPose = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0);
-        Pose2D targetPose = new Pose2D(DistanceUnit.INCH, 20, 20, AngleUnit.DEGREES, 45);
+        Pose2D targetPose = new Pose2D(DistanceUnit.INCH, 30, 0, AngleUnit.DEGREES, 0);
 
         PIDDrivetrain drive = new PIDDrivetrain(hardwareMap, telemetry, initialPose);
         drive.resetPosition(initialPose);
 
-        drive.addPathPoint(targetPose, 0.5, 2);
-        drive.addPathPoint(initialPose, 0.5, 2);
-        drive.addPathPoint(targetPose, 0.5, 2);
-        drive.addPathPoint(initialPose, 0.5, 2);
+        drive.addPathPoint(targetPose, 1.0, 2);
+        drive.addPathPoint(initialPose, 1.0, 2);
+        drive.addPathPoint(targetPose, 1.0, 2);
+        drive.addPathPoint(initialPose, 1.0, 2);
 
         telemetry.update();
 
@@ -43,19 +43,6 @@ public class PIDAutoTesting extends LinearOpMode {
             telemetry.addData("Target Heading", drive.getTargetPose().getHeading(AngleUnit.DEGREES));
             telemetry.addData("Current Heading", drive.getCurrentPose().getHeading(AngleUnit.DEGREES));
             telemetry.update();
-
-            drawField(drive);
         }
-    }
-
-    private void drawField(PIDDrivetrain drive) {
-        double x = drive.getCurrentPose().getX(DistanceUnit.INCH);
-        double y = drive.getCurrentPose().getY(DistanceUnit.INCH);
-        double heading = drive.getCurrentPose().getHeading(AngleUnit.DEGREES);
-        Pose2d rrPose = new Pose2d(x, y, Math.toRadians(heading));
-        TelemetryPacket packet = new TelemetryPacket();
-        packet.fieldOverlay().setStroke("#3F51B5");
-        Drawing.drawRobot(packet.fieldOverlay(), rrPose);
-        FtcDashboard.getInstance().sendTelemetryPacket(packet);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/PIDDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/PIDDrivetrain.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.drivetrain;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -10,20 +13,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.Drawing;
+import org.firstinspires.ftc.teamcode.util.BrainVector;
 import org.firstinspires.ftc.teamcode.util.PIDController;
 import org.firstinspires.ftc.teamcode.util.PathPoint;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 @Config
 public class PIDDrivetrain {
     public DcMotorEx leftFront, leftBack, rightBack, rightFront;
-    private Pose2D lastPinpointPose;
-    private Pose2D currentPose;
-    private Pose2D targetPose;
-    private PIDController forwardPIDControl;
-    private PIDController strafePIDControl;
-    private PIDController headPIDControl;
+    private Pose2D lastPinpointPose, currentPose, targetPose, leadPoint;
+    private double currentVelocity, currentAcceleration, previousVelocity, previousAcceleration,
+            previousForwardPower;
+    private long previousTimestamp;
+    private PIDController forwardPIDControl, strafePIDControl, headPIDControl;
     private Telemetry telemetry;
 
     private double calculatedLeftFrontPower, calculatedLeftBackPower, calculatedRightFrontPower,
@@ -31,17 +36,21 @@ public class PIDDrivetrain {
 
     public static class Params {
         public double forward_kP = 0.125;
-        public double forward_kI = 0.0025;
-        public double forward_kD = 0.01;
+        public double forward_kI = 0.05;
+        public double forward_kD = 0.02;
 
-        public double strafe_kP = 0.025;
-        public double strafe_kI = 0.0025;
-        public double strafe_kD = 0.005;
+        public double strafe_kP = 0.075;
+        public double strafe_kI = 0.05;
+        public double strafe_kD = 0.02;
 
-        public double head_kP = 0.025;
+        public double head_kP = 0.05;
         public double head_kI = 0.001;
         public double head_kD = 0.0;
 
+        public double kV = 0.1;
+        public double kA = 0.1;
+        public double kS = 0.1;
+        public double rampRate = 0.01;
 
         public double xOffset = 4.724;
         public double yOffset = 5.197;
@@ -55,6 +64,8 @@ public class PIDDrivetrain {
 
     private ArrayList<PathPoint> pathPoints;
     private PathPoint currentPathPoint;
+    private DistanceUnit distanceUnit = DistanceUnit.INCH;
+    private AngleUnit angleUnit = AngleUnit.DEGREES;
 
     public PIDDrivetrain(HardwareMap hardwareMap, Telemetry telemetry, Pose2D pose) {
         currentPose = pose;
@@ -103,18 +114,46 @@ public class PIDDrivetrain {
         pinpoint.setPosition(pose);
     }
 
+    private Pose2D queryVelocity() {
+        return pinpoint.getVelocity();
+    }
+
     private Pose2D queryPose() {
         return pinpoint.getPosition();
     }
 
     public Pose2D getCurrentPose() { return currentPose; }
 
+    public double getCurrentX() {
+        return getCurrentPose().getX(distanceUnit);
+    }
+
+    public double getCurrentY() {
+        return getCurrentPose().getY(distanceUnit);
+    }
+
+    public double getTargetX() {
+        return getTargetPose().getX(distanceUnit);
+    }
+
+    public double getTargetY() {
+        return getTargetPose().getY(distanceUnit);
+    }
+
+    public double getCurrentHeadingRadians() {
+        return getCurrentPose().getHeading(AngleUnit.RADIANS);
+    }
+
+    public double getTargetHeadingRadians() {
+        return getTargetPose().getHeading(AngleUnit.RADIANS);
+    }
+
     public Pose2D getTargetPose() { return targetPose; }
 
     private void moveToTargetPose() {
-        forwardPIDControl.setTarget(targetPose.getX(DistanceUnit.INCH));
-        strafePIDControl.setTarget(targetPose.getY(DistanceUnit.INCH));
-        headPIDControl.setTarget(targetPose.getHeading(AngleUnit.DEGREES));
+        forwardPIDControl.setTarget(getTargetX());
+        strafePIDControl.setTarget(getTargetY());
+        headPIDControl.setTarget(targetPose.getHeading(angleUnit));
         calculateMotorPowers();
 
         setDrivePower(calculatedLeftFrontPower, calculatedLeftBackPower, calculatedRightFrontPower,
@@ -125,28 +164,86 @@ public class PIDDrivetrain {
         pathPoints.add(new PathPoint(pathPoint, displacementTolerance, headingTolerance));
     }
 
-    public void drivePath() {
-        updatePoseEstimate();
-        PathPoint firstPathPoint = pathPoints.get(0);
-        if (firstPathPoint.inTolerance(currentPose)) {
-            pathPoints.remove(0);
-        }
-        currentPathPoint = pathPoints.get(0);
+    private void drawField() {
+        Pose2d currentPose = new Pose2d(getCurrentX(), getCurrentY(), getCurrentHeadingRadians());
+        TelemetryPacket packet = new TelemetryPacket();
 
-        setTargetPose(currentPathPoint.getPoint());
-        moveToTargetPose();
+        if (targetPose != null) {
+            Pose2d targetPose = new Pose2d(getTargetX(), getTargetY(), getTargetHeadingRadians());
+
+            packet.fieldOverlay().setStroke("#4CAF50");
+            Drawing.drawRobot(packet.fieldOverlay(), targetPose);
+        }
+
+        packet.fieldOverlay().setStroke("#3F51B5");
+        Drawing.drawRobot(packet.fieldOverlay(), currentPose);
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
+    }
+
+    public boolean drivePath() {
+        updatePoseEstimate();
+        drawField();
+        if (pathPoints.size() == 0) {
+            stop();
+            return true;
+        } else {
+            PathPoint firstPathPoint = pathPoints.get(0);
+            if (firstPathPoint.inTolerance(currentPose)) {
+                pathPoints.remove(0);
+            }
+
+            if (pathPoints.size() > 0) {
+                currentPathPoint = pathPoints.get(0);
+                leadPoint = calculateLeadPoint(currentPathPoint.getPoint());
+                setTargetPose(leadPoint);
+                moveToTargetPose();
+                return false;
+            } else {
+                stop();
+                return true;
+            }
+        }
+    }
+
+    private Pose2D calculateLeadPoint(Pose2D targetPoint) {
+        double x1 = currentPose.getX(distanceUnit);
+        double y1 = currentPose.getY(distanceUnit);
+        double x2 = targetPoint.getX(distanceUnit);
+        double y2 = targetPoint.getY(distanceUnit);
+
+        double distanceToGo = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double setPercentage = PARAMS.rampRate * currentVelocity;
+
+        BrainVector currentTargetPoint = new BrainVector(x1, y1);
+        BrainVector targetVectorPoint = new BrainVector(x2, y2);
+        BrainVector leadPoint = currentTargetPoint.calculateLeadPoint(targetVectorPoint, 0.01);
+
+        telemetry.addData("leadPoint X", leadPoint.getX());
+        telemetry.addData("leadPoint Y", leadPoint.getY());
+
+        return new Pose2D(distanceUnit, leadPoint.getX(), leadPoint.getY(), angleUnit, targetPoint.getHeading(angleUnit));
+    }
+
+    private boolean isReversePower(double forwardPower) {
+        return (forwardPower > 0 && previousForwardPower < 0) || (forwardPower < 0 && previousForwardPower > 0);
     }
 
     private void calculateMotorPowers() {
-        double forwardPower = -forwardPIDControl.update(currentPose.getX(DistanceUnit.INCH));
-        double strafePower =  strafePIDControl.update(currentPose.getY(DistanceUnit.INCH));
-        double headPower = headPIDControl.update(currentPose.getHeading(AngleUnit.DEGREES));
+        double feedForward = PARAMS.kV * currentVelocity + PARAMS.kA * currentAcceleration + PARAMS.kS;
 
-        headPIDControl.getTarget();
+        double forwardPower = -feedForward * forwardPIDControl.update(currentPose.getX(distanceUnit));
+        double strafePower =  strafePIDControl.update(currentPose.getY(distanceUnit));
+        double headPower = headPIDControl.update(currentPose.getHeading(angleUnit));
+
         telemetry.addData("headPIDControl target", headPIDControl.getTarget());
+        telemetry.addData("previousForwardPower", previousForwardPower);
+        telemetry.addData("forwardPower", forwardPower);
         telemetry.addData("headPower", headPower);
-        telemetry.addData("target heading", getTargetPose().getHeading(AngleUnit.DEGREES));
-        telemetry.addData("current heading", getCurrentPose().getHeading(AngleUnit.DEGREES));
+        telemetry.addData("target heading", getTargetPose().getHeading(angleUnit));
+        telemetry.addData("current heading", getCurrentPose().getHeading(angleUnit));
+        telemetry.addData("currentVelocity", currentVelocity);
+        telemetry.addData("currentAcceleration", currentAcceleration);
+        telemetry.addData("feedForward", feedForward);
 
         calculatedLeftFrontPower = forwardPower + strafePower + headPower;
         calculatedLeftBackPower = forwardPower - strafePower + headPower;
@@ -160,6 +257,8 @@ public class PIDDrivetrain {
 //        telemetry.addData("calculatedLeftBackPower", calculatedLeftBackPower);
 //        telemetry.addData("calculatedRightFrontPower", calculatedRightFrontPower);
 //        telemetry.addData("calculatedRightBackPower", calculatedRightBackPower);
+
+        previousForwardPower = forwardPower;
     }
 
     public Pose2D updatePoseEstimate() {
@@ -168,12 +267,20 @@ public class PIDDrivetrain {
         }
         pinpoint.update();
         currentPose = queryPose();
+        Pose2D velocityPose = queryVelocity();
+        double xVelocity = velocityPose.getX(distanceUnit);
+        double yVelocity = velocityPose.getY(distanceUnit);
+        currentVelocity = Math.sqrt(Math.pow(xVelocity, 2) + Math.pow(yVelocity, 2));
+        long currentTimestamp = System.currentTimeMillis();
+        currentAcceleration = (currentVelocity - previousVelocity) /
+                (currentTimestamp - previousTimestamp) / 1000;
+
+        previousVelocity = currentVelocity;
+        previousTimestamp = currentTimestamp;
         lastPinpointPose = currentPose;
 
-        return pinpoint.getVelocity();
+        return velocityPose;
     }
-
-
 
     public void setDrivePower(double leftFrontPower, double leftBackPower, double rightFrontPower,
                               double rightBackPower) {
